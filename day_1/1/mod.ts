c416a61b956ad94f5a3fc8d91102789e244e5f1f
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-12-01 06:09:39 +0100

    Day 1 done

---
+++ day_1/1/mod.ts
@@ -0,0 +1,17 @@
+const text = Deno.readTextFileSync('input.txt');
+const textByLine = text.split('\n');
+
+let buffer = 0;
+const dict = [];
+
+for (const line of textByLine) {
+  if (line === '') {
+    dict.push(buffer);
+    buffer = 0;
+  } else {
+    buffer += parseInt(line);
+  }
+}
+
+dict.sort((n1, n2) => n1 - n2);
+console.log(dict.pop());
