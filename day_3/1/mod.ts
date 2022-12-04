9424f625a8339c20d0479b3cd637a50b94e4085f
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-12-04 05:53:06 +0100

    day 3

---
+++ day_3/1/mod.ts
@@ -0,0 +1,21 @@
+const text = Deno.readTextFileSync("../input.txt");
+const textByLine = text.split("\n");
+
+const priorities = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let totalPrio = 0;
+
+for (const line of textByLine) {
+
+  const half1 = line.slice(0, line.length / 2);
+  const half2 = line.slice(line.length / 2, line.length);
+
+  for (const char of half1) {
+    if (half2.includes(char)) {
+      totalPrio += priorities.indexOf(char) + 1;
+      break;
+    }
+  }
+}
+
+console.log(totalPrio);
---
+++ day_3/2/mod.ts
@@ -0,0 +1,21 @@
+const text = Deno.readTextFileSync("../input.txt");
+const textByLine = text.split("\n");
+
+const priorities = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let totalPrio = 0;
+
+for (let index = 0; index <= textByLine.length - 3; index += 3) {
+  const lineOne = textByLine[index];
+  const lineTwo = textByLine[index + 1];
+  const lineThree = textByLine[index + 2];
+
+  for (const char of lineOne) {
+    if (lineTwo.includes(char) && lineThree.includes(char)) {
+      totalPrio += priorities.indexOf(char) + 1;
+      break;
+    }
+  }
+}
+
+console.log(totalPrio);
