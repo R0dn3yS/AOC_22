459ca74f8793a49ccfef4720a7c7222df02aa279
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-12-05 17:08:42 +0100

    day 5

---
+++ day_5/2/mod.ts
@@ -0,0 +1,64 @@
+const text = Deno.readTextFileSync('../input.txt');
+
+const initState = text.split('\n\n')[0].split('\n');
+const instructions = text.split('\n\n')[1].split('\n');
+
+const positionsToCheck = [1, 5, 9, 13, 17, 21, 25, 29, 33];
+
+const state: string[][] = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+];
+
+function checkLength(): number {
+  let i = 0;
+  for (const line of initState) {
+    if (line[1] === '1') break;
+    i++;
+  }
+  return i;
+}
+
+for (let i = checkLength() - 1; i >= 0; i--) {
+  for (const position of positionsToCheck) {
+    const letter = initState[i][position];
+    const posNum = positionsToCheck.indexOf(position);
+
+    if (letter !== ' ') {
+      state[posNum].push(letter);
+    }
+  }
+}
+
+for (const instruction of instructions) {
+  const newInstruction = instruction.split(' ');
+
+  const amount = parseInt(newInstruction[1]);
+  const from = parseInt(newInstruction[3]);
+  const to = parseInt(newInstruction[5]);
+
+  const buffer: string[] = [];
+
+  for (let i = 0; i < amount; i++) {
+    buffer.push(state[from - 1].pop()!);
+  }
+  
+  for (let i = 0; i < amount; i++) {
+    state[to - 1].push(buffer.pop()!);
+  }
+}
+
+let answer = '';
+
+for (const stack of state) {
+  answer += stack.pop();
+}
+
+console.log(answer);
