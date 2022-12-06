973484b3015b883ae97ecd277240455a70cee468
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-12-06 11:31:25 +0100

    day 6

---
+++ day_6/2/mod.ts
@@ -0,0 +1,18 @@
+const text = Deno.readTextFileSync('../input.txt');
+const str = text.split('');
+
+const charArr: string[] = [];
+
+for (let i = 0; i < 13; i++) {
+  charArr.push(str[i]);
+}
+
+for (let i = 3; i < str.length; i++) {
+  if (!charArr.includes(str[i]) && new Set(charArr).size === charArr.length) {
+    console.log(i + 1);
+    break;
+  } else {
+    charArr.shift();
+    charArr.push(str[i]);
+  }
+}
