de1fec5691d0440e31f6a5e7a5779fa30ed23146
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-12-09 14:52:40 +0100

    day 9 readable

--- day_9/1/mod.ts
+++ day_9/1/mod.ts
@@ -13,11 +13,16 @@ for (const [direction, count] of steps) {
     if (direction === 'U') knots[0].y--;
 
     for (let j = 1; j < knots.length; j++) {
-      const [H, T] = [knots[j - 1], knots[j]];
+      const [head, tail] = [knots[j - 1], knots[j]];
 
-      if (Math.abs(H.x - T.x) === 2 || Math.abs(H.y - T.y) === 2) {
-        T.x = H.x === T.x ? T.x : H.x > T.x ? T.x + 1 : T.x - 1;
-        T.y = H.y === T.y ? T.y : H.y > T.y ? T.y + 1 : T.y - 1;
+      if (Math.abs(head.x - tail.x) === 2 || Math.abs(head.y - tail.y) === 2) {
+        if (head.x !== tail.x) {
+          tail.x = head.x > tail.x ? tail.x + 1 : tail.x - 1;
+        }
+
+        if (head.y !== tail.y) {
+          tail.y = head.y > tail.y ? tail.y + 1 : tail.y - 1;
+        }
       }
     }
     visited.add(`${knots[len - 1].x},${knots[len - 1].y}`);
